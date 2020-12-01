d43a3d3a4bfc9fb23bf66eadf2e087da90db8a5a
AFelipeCR <andres.chaparro06@uptc.edu.co>
2020-11-30 23:47:32 -0500

    ServerB es cliente ahora

--- ServerB/index.js
+++ ServerB/index.js
@@ -1,10 +1,13 @@
-
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors({origin:true, credentials:true}))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port, () => {
