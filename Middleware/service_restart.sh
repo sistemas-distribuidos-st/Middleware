f4b8c7ce08559209fb5c36d6b6db81b2aa230635
DavidMa1021 <david.malaver01@uptc.edu.co>
2020-12-02 22:30:06 -0500

    bash ssh

---
+++ Middleware/service_restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "reboot service..."
+cd Lab
+git clone https://github.com/sistemas-distribuidos-st/Middleware.git
+cd Middleware/$1
+npm install
+echo "starting service..."
+pm2 start index.js
+echo "service restored..."
+
+
